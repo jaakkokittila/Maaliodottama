60a0c1a6e92e24e25e45b9cdecc4477a3a73695d
Jaakko Kittilä <jmkitt@utu.fi>
2022-02-03 23:29:45 +0200

    Improved shot gathering code and added code for transforming the coordinates for training

--- get_shots.py
+++ get_shots.py
@@ -1,12 +1,34 @@
 import requests
 import csv
+import pandas as pd
 
 seasons = ['2018', '2019', '2020', '2021', '2022']
 
+def get_shots():
+    try:
+        return pd.read_csv('data/shots.csv')
+    except:
+        return None
+
+def check_if_match_is_saved(match_id, shots):
+    if shots is None:
+        return False
+    else:
+        return len(shots[shots['Match_id'] == int(match_id)]) > 0
+
+saved_shots = get_shots()
+
+if saved_shots is None:
+    write_mode = 'w'
+else:
+    write_mode = 'a'
+
 field_names = ['Match_id', 'Type', 'Shooting_team', 'Left_team', 'Right_team', 'Period', 'Shot_x', 'Shot_y', 'Blocker', 'Shooter', 'Event_type']
-with open('shots.csv', mode='w', encoding='cp1252') as shot_file:
+with open('data/shots.csv', mode=write_mode, encoding='cp1252') as shot_file:
     shot_writer = csv.DictWriter(shot_file, fieldnames=field_names)
-    shot_writer.writeheader()
+
+    if write_mode == 'w':
+        shot_writer.writeheader()
 
     for season in seasons:
         url = url='https://liiga.fi/api/v1/games?tournament=runkosarja&season=' + season
@@ -20,28 +42,32 @@ with open('shots.csv', mode='w', encoding='cp1252') as shot_file:
                 match_ids.append(match['id'])
 
         for match_id in match_ids:
-            shot_base_url = 'https://liiga.fi/api/v1/shotmap/'
-            match_shot_url = shot_base_url + season + '/' + str(match_id)
+            match_id_with_season = str(match_id) + str(season)
+            match_saved = check_if_match_is_saved(match_id_with_season, saved_shots)
 
-            shot_data = requests.get(match_shot_url)
-            match_shots = shot_data.json()
+            if match_saved == False:
+                shot_base_url = 'https://liiga.fi/api/v1/shotmap/'
+                match_shot_url = shot_base_url + season + '/' + str(match_id)
 
-            for shot in match_shots:
-                event_type = shot['eventType']
+                shot_data = requests.get(match_shot_url)
+                match_shots = shot_data.json()
 
-                if event_type != 'PLAYER_BLOCKED':
-                    shot_type = shot['type']
-                    shooting_team = shot['shootingTeamId']
-                    left_team = shot['leftTeam']
-                    right_team = shot['rightTeam']
-                    period = shot['period']
-                    shot_x = shot['shotX']
-                    shot_y = shot['shotY']
-                    blocker = shot['blockerId']
-                    shooter = shot['shooterId']
+                for shot in match_shots:
+                    event_type = shot['eventType']
 
-                    shot_writer.writerow({'Match_id': str (match_id) + str(season), 'Type': shot_type, 'Shooting_team': shooting_team, 'Left_team': left_team, 'Right_team': right_team,
-                                          'Period': period, 'Shot_x': shot_x, 'Shot_y': shot_y, 'Blocker': blocker, 'Shooter': shooter, 'Event_type': event_type})
+                    if event_type != 'PLAYER_BLOCKED':
+                        shot_type = shot['type']
+                        shooting_team = shot['shootingTeamId']
+                        left_team = shot['leftTeam']
+                        right_team = shot['rightTeam']
+                        period = shot['period']
+                        shot_x = shot['shotX']
+                        shot_y = shot['shotY']
+                        blocker = shot['blockerId']
+                        shooter = shot['shooterId']
+
+                        shot_writer.writerow({'Match_id': match_id_with_season, 'Type': shot_type, 'Shooting_team': shooting_team, 'Left_team': left_team, 'Right_team': right_team,
+                                              'Period': period, 'Shot_x': shot_x, 'Shot_y': shot_y, 'Blocker': blocker, 'Shooter': shooter, 'Event_type': event_type})
 
     shot_file.close()
 
---
+++ transform_shots_to_one_end.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+shots = pd.read_csv('data/shots.csv', encoding='cp1252')
+
+def transform_coordinates_to_one_end(match_shots):
+    left_team_id = match_shots.iloc[0]['Left_team']
+    right_team_id = match_shots.iloc[0]['Right_team']
+
+    for i, row in match_shots.iterrows():
+        if row['Shooting_team'] == left_team_id and (row['Period'] == 1 or row['Period'] == 3) or row['Shooting_team'] == right_team_id and row['Period'] == 2:
+            new_x = -1 * (row['Shot_x'] - 1030 )
+            shots.loc[i, 'Shot_x'] = new_x
+            new_y = -1 * (row['Shot_y'] - 515 )
+            shots.loc[i, 'Shot_y'] = new_y
+
+match_ids = shots.Match_id.unique()
+
+for match_id in match_ids:
+    match_shots = shots[(shots['Match_id'] == match_id)]
+    transform_coordinates_to_one_end(match_shots)
+
+shots.to_csv('data/transformed_shots.csv', index=False)
